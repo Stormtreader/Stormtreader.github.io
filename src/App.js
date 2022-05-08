8b16b74e2a3bac2ed2ac53a7381a5c3e14cf19b6
Stormtreader <ainemorgan_uk@hotmail.com>
2022-05-08 11:42:12 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const GridHeader = () => {
+  return <div class="header">WORDISH</div>
+}
+
+/**
+ * 
+ * @param letter Single character value | undefined
+ * @param isCorrect no | yes | elsewhere (required)
+ * @returns 
+ */
+const Character = ({ letter, isCorrect }) => {
+  const cssClasses = 'character ' + isCorrect
+  return <div class={cssClasses}>{letter}</div>
+}
+
+const GuessLine = ({ answer, word }) => {
+  return <div class="line">
+    {word.split('').map((letter, index) => {
+      const isCorrect = letter === answer[index]
+        ? 'yes'
+        : answer.indexOf(letter) !== -1
+          ? 'elsewhere'
+          : 'no'
+
+      return <Character letter={letter} isCorrect={isCorrect} />
+    })}
+  </div>
+}
+
+const RemainingLine = () => {
+  return <div class="line">
+    <div class="character"></div>
+    <div class="character"></div>
+    <div class="character"></div>
+    <div class="character"></div>
+    <div class="character"></div>
+  </div>
+}
+
+const App = () => {
+
+  const answer = 'XXOIT'
+  const guesses = [
+    'ABOUT',
+    'PRINT',
+  ]
+  const maxGuesses = 6
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="page">
+      <div class="grid">
+        <GridHeader />
+        {guesses.map(guess => <GuessLine answer={answer} word={guess} />)}
+        {Array(maxGuesses - guesses.length).fill('').map(() => <RemainingLine />)}
+      </div>
     </div>
   );
 }
